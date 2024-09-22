d397e4b126afd9e0cc102143978c7578c15488f5
jfernando02 <jfernando020202@gmail.com>
2024-09-22 17:25:20 +1000

    Reduced dataset to 1000 images, restored original model

--- scripts/models.py
+++ scripts/models.py
@@ -6,11 +6,11 @@ from torchvision.models import vit_h_14, ViT_H_14_Weights
 class BMIHead(nn.Module):
     def __init__(self):
         super(BMIHead, self).__init__()
-        self.linear1 = nn.Linear(1280, 320)
-        self.linear2 = nn.Linear(320, 80)
-        self.linear3 = nn.Linear(80, 1)
-        #self.linear4 = nn.Linear(160, 80)
-        #self.linear5 = nn.Linear(80, 1)
+        self.linear1 = nn.Linear(1280, 640)
+        self.linear2 = nn.Linear(640, 320)
+        self.linear3 = nn.Linear(320, 160)
+        self.linear4 = nn.Linear(160, 80)
+        self.linear5 = nn.Linear(80, 1)
         self.gelu = nn.GELU()
         self.dropout = nn.Dropout(0.5)
 
@@ -21,10 +21,10 @@ class BMIHead(nn.Module):
         x = self.linear2(x)
         x = self.gelu(x)
         x = self.linear3(x)
-        #x = self.gelu(x)
-        #x = self.linear4(x)
-        #x = self.gelu(x)
-        #x = self.linear5(x)
+        x = self.gelu(x)
+        x = self.linear4(x)
+        x = self.gelu(x)
+        x = self.linear5(x)
         out = self.gelu(x)
         return out
 
